1a704827bb29d0b4952be2618015f266a9a39a69
Julio Vedovatto <juliovedovatto@gmail.com>
2021-02-22 21:58:32 -0300

    feat(core): routing support added
    - vue-router@4 added
    - basic router file added
    - main application file updated with router definitions
    - Home view added (wip)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import viteESLint from '@ehutch79/vite-eslint'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteESLint(), vue()]
+  plugins: [viteESLint(), vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') }
+    ]
+  }
 })

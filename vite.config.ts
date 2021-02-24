c3068393b1d7762ad81bc1d6d946d2580998ea94
Julio Vedovatto <juliovedovatto@gmail.com>
2021-02-23 22:05:52 -0300

    fix(UI): Tailwind preflight CSS disabled
    - preflight is not beneficial at this time
    - vite config files improvements

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import ViteESLint from '@ehutch79/vite-eslint'
 import Vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import { defineConfig } from 'vite'
-import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ViteESLint(), WindiCSS(),Vue()],
+  plugins: [
+    ViteESLint(),
+    WindiCSS({ preflight: false }),
+    Vue()
+  ],
   resolve: {
     alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') }
+      { find: '@', replacement: resolve(__dirname, 'src') }
     ]
   },
   css: {

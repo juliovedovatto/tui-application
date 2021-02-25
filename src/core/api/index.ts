65ef098bbbbd49c63f771a6e9e671f3e0bab5fc5
Julio Vedovatto <juliovedovatto@gmail.com>
2021-02-24 23:47:27 -0300

    feat(api): Hotels API support added (WIP)
    - relying on Amadeus API to query info

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,3 +1,2 @@
-//  export { default as Foo } from './modules/Foo'
-
 export { default as Weather } from './modules/weather'
+export { default as Hotels } from './modules/hotels'
---
+++ src/core/api/modules/hotels.ts
@@ -0,0 +1,82 @@
+import { HOTEL_API_KEY, HOTEL_API_SECRET } from '@/core/config'
+
+import API from '../main'
+import { objToParams } from '@/core/helpers/url'
+
+/**
+ * Hotel API Class. Using Amadeus API
+ * @note Amadeus Node SDK is not fully compatible with browsers, there was a need to make more direct calls, relying on axios package
+ */
+export class Hotels extends API {
+  private accessToken: Maybe<string> = null
+
+  readonly BLACKLIST = ['/v1/security/oauth2/token']
+
+  constructor() {
+    super()
+
+    this.setup()
+  }
+
+  async setup(): Promise<void> {
+    const self = this
+
+    this.request.defaults.baseURL = 'https://test.api.amadeus.com/'
+    this.request.interceptors.request.use(config => {
+      config.headers = {
+        ...config.headers,
+        ...(this.accessToken && { Authorization: `Bearer ${this.accessToken}` })
+      }
+
+      return config
+    })
+    this.request.interceptors.response.use(
+      response => response,
+      async function (err) {
+        const { status = null } = err?.response || {}
+
+        if (status === 401) {
+          const { config } = err
+          const { __isRetryRequest, url } = config
+
+          if (!self.BLACKLIST.includes(url)) {
+            if (!__isRetryRequest) {
+              await self.auth()
+              return setTimeout(() => self.request({ ...config, __isRetryRequest: true }), 300)
+            }
+          }
+        }
+
+        return Promise.reject(err)
+      }
+    )
+  }
+
+  async preflight(): Promise<void> {
+    if (!this.accessToken) {
+      await this.auth()
+    }
+  }
+
+  async auth(): Promise<void> {
+    this.accessToken = null
+
+    const { data: { access_token } } = await this.request.post('v1/security/oauth2/token', objToParams({
+      grant_type: 'client_credentials',
+      client_id: HOTEL_API_KEY,
+      client_secret: HOTEL_API_SECRET
+    }))
+
+    this.accessToken = access_token
+  }
+
+  async fetchCity(city: string): Promise<PlainObject[]> {
+    await this.preflight();
+
+    const { data } = await this.request.get('v2/shopping/hotel-offers', { params: { cityCode: city } })
+
+    return data
+  }
+}
+
+export default new Hotels()

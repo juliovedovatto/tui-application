60792e685194d25abe1b024ea18f33c315f6fc8d
Julio Vedovatto <juliovedovatto@gmail.com>
2021-02-24 23:44:57 -0300

    feat(api): Weather API improvements
    - custom enum added with allowed countries
    - fetchCity method added

--- src/core/api/modules/weather.ts
+++ src/core/api/modules/weather.ts
@@ -1,8 +1,28 @@
 import API from '../main'
+import { APIResponse } from 'typings/api'
+import { WEATHER_API_KEY } from '@/core/config'
+
+export enum COUNTRY_CODES {
+  PT,
+  BR,
+  ES,
+  IT
+}
 
 export class Weather extends API {
   constructor() {
     super()
+
+    this.request.defaults.baseURL = 'https://api.openweathermap.org/data/2.5'
+    this.request.defaults.params = { ...this.request.defaults.params, appid: WEATHER_API_KEY }
+  }
+
+  async fetchCity(city: string, countryCode: Maybe<COUNTRY_CODES>): APIResponse {
+    const q = `${city}${countryCode ? `,${countryCode}` : ''}`
+
+    const { data } = await this.request.get('/weather', { params: { q } })
+
+    return data
   }
 }
 

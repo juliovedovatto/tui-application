094e9dfca6114bd471595edf3b05bbe74aeec759
Julio Vedovatto <juliovedovatto@gmail.com>
2021-02-26 20:32:43 -0300

    feat(components): Weather List component added (WIP)

---
+++ src/components/lists/WeatherList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="weather__list">
+    <div class="flex">
+      <div :key="`weather-${i}`" class="text-center mr-4 weather" v-for="(item, i) in items">
+        <time :datetime="item.date">{{ formatLocale(item.date) }}</time>
+        <picture class="block">
+          <img :src="item.icon" />
+        </picture>
+        <strong>{{ item.text }}</strong>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup name="HotelsList" lang="ts">
+// TODO: add tooltip to show full text
+
+import { defineProps } from "vue"
+
+import { formatLocale } from '@/core/helpers/date'
+
+const props = defineProps({
+  items: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.hotels__list {
+  @include e('item') {
+    grid-template-columns: minmax(auto, 400px) auto;
+  }
+}
+</style>
--- src/components/lists/index.ts
+++ src/components/lists/index.ts
@@ -1,1 +1,2 @@
 export { default as HotelsList } from './HotelsList.vue'
+export { default as WeatherList } from './WeatherList.vue'

293ba3219f654f6651a2a0930ccee43fa2984b3c
Julio Vedovatto <juliovedovatto@gmail.com>
2021-02-26 20:30:16 -0300

    feat(helpers): date helper added
    - relying on Luxon to format dates

---
+++ src/core/helpers/date.ts
@@ -0,0 +1,9 @@
+import { DateTime } from 'luxon'
+
+export function format(date: string, format: string): string {
+  return DateTime.fromISO(date).toFormat(format)
+}
+
+export function formatLocale(date: string, locale: string): string {
+  return format(date, 'LL/dd/yyyy')
+}

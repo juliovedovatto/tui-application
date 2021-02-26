078dad1478156108bb81cc4453f4b5ad62b1b32b
Julio Vedovatto <juliovedovatto@gmail.com>
2021-02-26 20:33:12 -0300

    feat: Weather component added to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <h1>Home</h1>
 
   <div>
+    <weather-list :items="weather" />
     <hotels-list :items="offers" />
   </div>
 </template>
@@ -9,14 +10,18 @@
 <script setup lang="ts">
   import { useStore } from '@/store'
   import { onBeforeMount, computed } from 'vue'
-  import { HotelsList } from '@/components/lists'
+  import { HotelsList, WeatherList } from '@/components/lists'
 
   const store = useStore()
 
   const offers = computed(() => store.getters['hotels/items'])
+  const weather = computed(() => store.getters['weather/forecast'])
 
   onBeforeMount(async () => {
     store.dispatch('hotels/clear')
+    store.dispatch('weather/clear')
+
+    await store.dispatch('weather/list', { city: 'Paris' })
     await store.dispatch('hotels/list', { cityCode: 'PAR', sort: 'PRICE' })
   })
 </script>

e9c44bec5d4d8fb6d7a1f6a7cddd8acf01364abf
Julio Vedovatto <juliovedovatto@gmail.com>
2021-03-01 18:53:24 -0300

    feat(component): Loading component added

---
+++ src/components/markup/LoadingMarkup.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="loading-markup">
+  <img :src="LoadingMarkupImage" alt="Loading..." class="loading-markup__image" :style="imageStyle" />
+  <div class="loading-markup__message" v-if="message">{{ message }}</div>
+</div>
+</template>
+
+<script setup name="LoadingMarkup" lang="ts">
+import { computed, defineProps } from 'vue'
+import LoadingMarkupImage from '@/assets/images/markup/loader.svg'
+
+const props = defineProps({
+  message: {
+    type: String,
+    default: ''
+  },
+  size: {
+    type: Number,
+    default: 100
+  }
+})
+
+const imageStyle = computed(() => ({
+  width: `${props.size}px`
+}))
+
+</script>
+
+<style lang="scss" scoped>
+.loading-markup {
+  @apply text-center;
+
+  @include e('image') {}
+  @include e('message') {
+    @apply italic mt-4;
+
+    color: $tui-grey-50;
+  }
+}
+</style>
---
+++ src/components/markup/index.ts
@@ -0,0 +1,1 @@
+export { default as LoadingMarkup } from './LoadingMarkup.vue'
